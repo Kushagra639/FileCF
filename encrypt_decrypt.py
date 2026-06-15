abbfce93eb0425138c9d2ff2cb9dd12775fb814f
Kushagra Aggarwal <167755405+Kushagra639@users.noreply.github.com>
2026-06-15 12:41:40 +0530

    Uploaded main.py and encrypt_decrypt.py files

---
+++ encrypt_decrypt.py
@@ -0,0 +1,58 @@
+import os
+import base64
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+
+def generate_key(password, salt):
+
+    password = password.encode()
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=salt,
+        iterations=480000,
+    )
+
+    key = base64.urlsafe_b64encode(
+        kdf.derive(password)
+    )
+
+    return key
+
+def encrypt_file(file, password):
+    salt = os.urandom(16)
+    key = generate_key(password, salt)
+    fernet = Fernet(key)
+    
+    with open(file, 'rb') as f:
+        original = f.read()
+    
+    encrypted = fernet.encrypt(original)
+
+    with open(file+".encrypted", 'wb') as fn:
+        fn.write(salt)
+        fn.write(encrypted)
+
+    print("Encryption successful.\nEncrypted file saved as: " + file + ".encrypted")
+
+def decrypt_file(file, password):
+    with open(file, 'rb') as f:
+        salt = f.read(16)
+        encrypted = f.read()
+    
+    key = generate_key(password, salt)
+    fernet = Fernet(key)
+
+    try:
+        decrypted = fernet.decrypt(encrypted)
+
+        with open(file.replace(".encrypted", ""), 'wb') as fn:
+            fn.write(decrypted)
+
+        print("Decryption successful.\nFile saved as: " + file.replace(".encrypted", ""))
+    
+    except Exception as e:
+        print("Decryption failed. Check your password and try again.")
+        print(f"Error: {e}")
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from encrypt_decrypt import encrypt_file
+from encrypt_decrypt import decrypt_file
+
+while True:
+    print("=======================================")
+    print("           Secure File Locker          ")
+    print("=======================================")
+    print("1. Encrypt File")
+    print("2. Decrypt File")
+    print("3. Exit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        file = input("Enter the path of the file to encrypt (with the file name and extension): ")
+        file = file.strip()
+        file = file.strip('"')
+        password = input("Enter the password: ")
+        encrypt_file(file, password)
+    elif choice == "2":
+        file = input("Enter the path of the file to decrypt (with the file name and extension): ")
+        file = file.strip()
+        file = file.strip('"')
+        password = input("Enter the password: ")
+        decrypt_file(file, password)
+    elif choice == "3":
+        break
+    else:
+        print("Invalid choice. Please try again.")
