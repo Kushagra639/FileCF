98e75c32cfb317ced8dd2c0646a3ff68b910a5b6
Kushagra Aggarwal <167755405+Kushagra639@users.noreply.github.com>
2026-06-15 13:03:01 +0530

    Replace password input with getpass, changed variable "file"
    Replaced password input from default string input to getpass for security. Changes the variable "file" to "filename" for better compatibility.

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import os
 from encrypt_decrypt import encrypt_file
 from encrypt_decrypt import decrypt_file
+from getpass import getpass
 
 while True:
-    print("=======================================")
-    print("           Secure File Locker          ")
-    print("=======================================")
+    print("="*40)
+    print("           Secure File Locker")
+    print("="*40)
     print("1. Encrypt File")
     print("2. Decrypt File")
     print("3. Exit")
     choice = input("Enter your choice: ")
 
     if choice == "1":
-        file = input("Enter the path of the file to encrypt (with the file name and extension): ")
-        file = file.strip()
-        file = file.strip('"')
-        if not os.path.isfile(file):
+        filename = input("Enter the path of the file to encrypt (with the file name and extension): ")
+        filename = filename.strip()
+        filename = filename.strip('"')
+        if not os.path.isfile(filename):
             print("File not found.")
             continue
-        password = input("Enter the password: ")
-        encrypt_file(file, password)
+        password = getpass("Enter the password: ")
+        encrypt_file(filename, password)
     elif choice == "2":
-        file = input("Enter the path of the file to decrypt (with the file name and extension): ")
-        file = file.strip()
-        file = file.strip('"')
-        if not os.path.isfile(file):
+        filename = input("Enter the path of the file to decrypt (with the file name and extension): ")
+        filename = filename.strip()
+        filename = filename.strip('"')
+        if not os.path.isfile(filename):
             print("File not found.")
             continue
-        password = input("Enter the password: ")
-        decrypt_file(file, password)
+        password = getpass("Enter the password: ")
+        decrypt_file(filename, password)
     elif choice == "3":
         break
     else:
